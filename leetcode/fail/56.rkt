c0c4e70cbf899af025326168650e1146f32ff7d2
lilin3035 <51811397+lilin3035@users.noreply.github.com>
2021-08-07 02:13:15 +0000

    Update and rename leetcode/56.rkt to leetcode/fail/56.rkt

--- leetcode/fail/56.rkt
+++ leetcode/fail/56.rkt
@@ -1,10 +1,17 @@
 (define/contract (merge intervals)
   (-> (listof (listof exact-integer?)) (listof (listof exact-integer?)))
-(letrec ((readuce (lambda (intervals)
+(begin 
+(define (cache index)
+(let ((readuce (eval (car index)))(x (car (cdr index))))
+(if (equal? (readuce x)  (readuce (readuce x)))
+(readuce x)
+(cache (list (car index) (readuce x))))))
+(define (listsort intervals)
+(letrec ((cont
+(lambda (n)
 (if (< (length intervals) 2)
 intervals
 (let ((intervals (sort intervals #:key car <)))
-(letrec ((cont (lambda (n)
 (let (
 (a (car (list-ref intervals (- n 1))))
 (b (car (cdr (list-ref intervals (- n 1)))))
@@ -46,11 +53,18 @@ intervals
             '()
             (cont (add1 n))))
 ]
-))))))
-(cont 1)))))
-))
-(if (equal? (readuce intervals) (readuce (readuce intervals)))
-    (readuce intervals)
-    (readuce (readuce intervals)))
+)))))))) (cont 1)))
+(cache (list listsort intervals)))
 )
-  )
+
+#|
+?: literal data is not allowed;
+ no #%datum syntax transformer is bound
+  at: #<procedure:listsort>
+  context...:
+   solution.rkt:1:0: merge
+   /usr/share/racket/collects/racket/contract/private/arrow-higher-order.rkt:375:33
+   solution.rkt:84:2
+   body of (submod "solution.rkt" main)
+|#
+;;这道题意料之外的输入比较多，我反复修改了几次条件，最终得到这个程序时，在自己的电脑上可以正常求出几个例子，但是在leetcode中发生意料之外的错误，不清楚原因
