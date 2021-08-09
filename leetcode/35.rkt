6fcdce87e8720331b85c828c58988c0e96e11d72
lilin3035 <51811397+lilin3035@users.noreply.github.com>
2021-08-09 12:27:41 +0000

    Create 35.rkt

---
+++ leetcode/35.rkt
@@ -0,0 +1,14 @@
+(define/contract (search-insert nums target)
+      (-> (listof exact-integer?) exact-integer? exact-integer?)
+        (cond
+        ((> target (list-ref nums (- (length nums) 1))) (length nums))
+        ((= target (list-ref nums (- (length nums) 1))) (- (length nums) 1))
+        ((<= target (list-ref nums 0)) 0)
+        (#t (letrec ((search (lambda (n)
+        (cond
+        ((= (list-ref nums n) target) n)
+        ((and (< target (list-ref nums n)) (> target (list-ref nums (- n 1))))  n)
+
+        ((< target (list-ref nums (- (length nums) 1))) (search(add1 n)))))))
+        (search 1))))
+      )
