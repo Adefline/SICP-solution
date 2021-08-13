35c2f9e143d19a8546624058e84e98ed54fc6956
lilin3035 <51811397+lilin3035@users.noreply.github.com>
2021-08-13 01:15:31 +0000

    Delete leetcode directory

--- leetcode/TLE/1.rkt
+++
@@ -1,4 +0,0 @@
-(define/contract (two-sum nums target)
-  (-> (listof exact-integer?) exact-integer? (listof exact-integer?))
-    (filter (lambda (n) (member (- target (list-ref nums n)) (remove (list-ref nums n) nums))) (build-list (length nums) values))
-)
--- leetcode/TLE/1137.rkt
+++
@@ -1,8 +0,0 @@
-(define/contract (tribonacci n)
-  (-> exact-integer? exact-integer?)
-(cond
-[(= n 0) 0]
-[(< n 3) 1]
-[(< 2 n) (+ (tribonacci (- n 1)) (tribonacci (- n 2)) (tribonacci (- n 3)))]
-)
-  )
